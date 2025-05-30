22082f0f01f39ab8b7e0def73dfcb477084353ba
CustomCreations2025 <drcustomcreations2025@gmail.com>
2025-05-30 09:33:34 +0100

    Update Booking.tsx

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -101,7 +101,7 @@ const Booking = () => {
                   >
                     {/* Hidden Formsubmit fields */}
                     <input type="hidden" name="_captcha" value="false" />
-                    <input type="hidden" name="_next" value="https://www.mistressmya.world/thank-you.html" />
+                    <input type="hidden" name="_next" value="https://mistressmya.world/thank-you.html" />
                     <input type="hidden" name="_autoresponse" value="Thank you for your booking request! I will get back to you shortly. If you need urgent assistance, please email welcome2myasworld@gmail.com." />
                     
                     {/* Error Message */}
